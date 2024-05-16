141681f25996b8eb98cf840ede3c3342dc081d67
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-16 17:42:41 +0400

    feat: create pages, create components footer/navbar and routes and create avis.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ActivitiesPage from "./pages/ActivitiesPage.jsx";
+import DetailPage from "./pages/DetailPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/About",
+        element: <AboutPage />,
+      },
+      {
+        path: "/Activities",
+        element: <ActivitiesPage />,
+      },
+      {
+        path: "/Detail",
+        element: <DetailPage/>,
+      },
+      {
+        path: "*",
+        element: <h1> Cette page n`existe pas! </h1> ,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
