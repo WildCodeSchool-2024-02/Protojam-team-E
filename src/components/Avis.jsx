0f25a14a3e70804a0b44f91f0ce76a38f43b2347
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-16 21:34:35 +0400

    Add button to display all notices in desktop version and little css

--- src/components/Avis.jsx
+++ src/components/Avis.jsx
@@ -1,5 +1,6 @@
 import avis from "../data/avis";
 import "../css/Avis.css";
+import { useState } from "react";
 
 function avisRandom(avis) {
   const randomIndex = Math.floor(Math.random() * avis.length);
@@ -7,17 +8,44 @@ function avisRandom(avis) {
 }
 
 export default function Avis() {
+  const [showAllAvis, setShowAllAvis] = useState(false);
   const randomAvis = avisRandom(avis);
 
+  const toggleAvis = () => {
+    setShowAllAvis(!showAllAvis);
+  };
+
+  const renderAvis = () => {
+    if (showAllAvis) {
+      return avis.map((avis) => (
+        <div key={avis.id} className="container-avis">
+          <p>{avis.title}</p>
+          <p>Utilisateur : {avis.name}</p>
+          <p>{avis.rating}</p>
+          <p>{avis.description}</p>
+        </div>
+      ));
+    } else {
+      return (
+        <div className="avis">
+          <h2>Les avis de nos utilisateurs</h2>
+          <div className="container-avis">
+            <p>{randomAvis.title}</p>
+            <p>Utilisateur : {randomAvis.name}</p>
+            <p>{randomAvis.rating}</p>
+            <p>{randomAvis.description}</p>
+          </div>
+        </div>
+      );
+    }
+  };
+  
   return (
-    <div className="avis">
-      <h2>Les avis de nos utilisateurs</h2>
-    <div className="container-avis">
-      <p>{randomAvis.title}</p>
-      <p>Utilisateur : {randomAvis.name}</p>
-      <p>{randomAvis.rating}</p>
-      <p>{randomAvis.description}</p>
-    </div>
+    <div className="button">
+      {renderAvis()}
+      <button onClick={toggleAvis}>
+        {showAllAvis ? "Moins d'avis" : "Tous les avis"}
+      </button>
     </div>
   );
 }
