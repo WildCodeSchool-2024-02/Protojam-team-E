2608ac0408b7dcd02f8b5df39c8be20979bc31f4
David L <159317073+daviscrib@users.noreply.github.com>
2024-05-17 01:11:26 +0200

    Revert "feat: Add menuburger in App.jsx and  navBar.css"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"
 function App() {
   return (
-    <div className="bg-FDFDF2">
+    <>
       <Navbar />
       <Outlet />
       <Footer />
-    </div>
+    </>
   );
 }
 
