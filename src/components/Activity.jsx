e24a2ef9ce8ab6c5bc855f7b725ec9d384fcfabe
Chaymae El Hayani <layani.chay@gmail.com>
2024-05-16 19:12:51 +0200

    Update props in activity

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -6,8 +6,11 @@ export default function Activity({activity}) {
             <div className="font-bold text-2xl mb-2"> {activity.name}</div>  
             <p className="text-gray-700 text-base"> Rating: {activity.rating} ⭐</p> </div>
         <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gradient-to-br from-green-500 via-green-300 to-yellow-200 rounded-full px-3 py-1 text-sm font-semibold ">
+            <span className="inline-block bg-gradient-to-br from-green-500 via-green-300 to-yellow-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-3 ">
             #{activity.category}
+            </span>
+            <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold ">
+            🕐{activity.access}
             </span>
             </div>  
         </div>
