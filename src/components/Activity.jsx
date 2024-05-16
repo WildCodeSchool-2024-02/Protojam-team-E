4d35fcc791dc2755f9b939102bf204fae7effb17
unknown <adj.hamzaoui@gmail.com>
2024-05-17 00:28:40 +0200

    Description de vos modifications

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,6 +1,6 @@
 export default function Activity({activity}) {
     return(
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="w-80 max-w-sm rounded overflow-hidden shadow-lg">
             <img className="w-full h-80 w-76 object-cover"  src={activity.image} alt={activity.name} />
             <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2"> {activity.name}</div>  
