82afd6ec0da256a77df7f65157755d0b394c721b
Chaymae El Hayani <layani.chay@gmail.com>
2024-05-16 17:21:56 +0200

    feat: activityList and activity component

--- src/pages/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage.jsx
@@ -1,7 +1,13 @@
+import ActivitiesList from "../components/ActivitiesList";
+
 export default function ActivitiesPage() {
     return (
     <>
     <h1> Activities Page</h1>
+    
+    
+    <ActivitiesList/>
+
     </>
     )
   }
