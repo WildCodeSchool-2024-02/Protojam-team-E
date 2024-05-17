479d8b151e86c668d896e156ec41f989edd4bf6c
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-17 13:31:49 +0400

    Add links to carousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -10,6 +12,18 @@ import "swiper/css/navigation";
 import { Keyboard, Pagination, Navigation } from "swiper/modules";
 
 export default function Carrousel() {
+  const [activities, setActivities] = useState ();
+
+  useEffect (() => {
+    fetch("/src/data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setActivities(data);
+      });
+  }, []);
+
+  console.info(activities);
+
   return (
     <>
       <Swiper
@@ -33,7 +47,7 @@ export default function Carrousel() {
         <div className="h-dvh py-16 px-6 ">
           <SwiperSlide>
             <div>
-              <a href="">
+              <Link to={`/activities/1`}>
                 <p className="absolute top-10 backdrop-blur-sm left-0 w-full text-white text-center text-3xl p-2">
                   Retraite de Yoga en Forêt
                 </p>
@@ -45,28 +59,28 @@ export default function Carrousel() {
                 <p className="absolute bottom-10 backdrop-blur-sm left-0 w-full text-white drop-shadow-2xl text-center text-3xl p-1">
                   Note : 4.9/5 ⭐️
                 </p>
-              </a>
+                </Link>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div>
-              <a href="">
+              <Link to={`/activities/2`}>
                 <p className="absolute top-10 backdrop-blur-sm left-0 w-full text-white text-center text-3xl p-2">
                   Sortie escalade sur blocs
                 </p>
                 <img
                   src="https://images.unsplash.com/photo-1507034589631-9433cc6bc453?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="Femme qui fait de l'escalade"
-                />
+                  />
                 <p className="absolute bottom-10 backdrop-blur-sm left-0 w-full text-white drop-shadow-2xl text-center text-3xl p-1">
                   Note : 4.9/5 ⭐️
                 </p>
-              </a>
+                  </Link>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div>
-              <a href="">
+            <Link to={`/activities/3`}>
                 <p className="absolute top-10 backdrop-blur-sm left-0 w-full text-white text-center text-3xl p-2">
                   Baie de la Somme
                 </p>
@@ -77,12 +91,12 @@ export default function Carrousel() {
                 <p className="absolute bottom-10 backdrop-blur-sm left-0 w-full text-white drop-shadow-2xl text-center text-3xl p-1">
                   Note : 4.5/5 ⭐️
                 </p>
-              </a>
+            </Link>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div>
-              <a href="">
+            <Link to={`/activities/10`}>
                 <p className="absolute top-10 backdrop-blur-sm left-0 w-full text-white text-center text-3xl p-2">
                   Sortie à vélo le long de la Coulée Verte Sud
                 </p>
@@ -93,12 +107,13 @@ export default function Carrousel() {
                 <p className="absolute bottom-10 backdrop-blur-sm left-0 w-full text-white drop-shadow-2xl text-center text-3xl p-1">
                   Note : 4.6/5 ⭐️
                 </p>
-              </a>
+            </Link>
+            
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div>
-              <a href="">
+            <Link to={`/activities/7`}>
                 <p className="absolute top-10 backdrop-blur-sm left-0 w-full text-white text-center text-3xl p-2">
                   Jardin des plantes
                 </p>
@@ -109,7 +124,7 @@ export default function Carrousel() {
                 <p className="absolute bottom-10 backdrop-blur-sm left-0 w-full text-white drop-shadow-2xl text-center text-3xl p-1">
                   Note : 4.7/5 ⭐️
                 </p>
-              </a>
+              </Link>
             </div>
           </SwiperSlide>
         </div>
