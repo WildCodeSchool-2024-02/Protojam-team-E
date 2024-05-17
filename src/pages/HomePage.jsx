70789c8d0f2eede08e7e10c107c498b72c696170
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-17 08:58:32 +0400

    Modification of description in homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,22 +4,27 @@ import HomeActivity from "../components/HomeActivity";
 export default function HomePage() {
   return (
     <>
-      <p className="py-10 px-6 text-justify md:mx-40">
-        Chez CheminVert, nous offrons des escapades en nature pour rééquilibrer
-        votre énergie intérieur. Que vous cherchiez aventure, calme ou
-        contemplation, nos activités répondent à vos besoin. Chaque escapade est
-        une occasion de renouer avec vous-même et la nature.
+      <p className="py-10 px-10 text-justify md:mx-40">
+        Avec CheminVert, nous vous proposons une expérience complète dans la
+        nature, pour rééquilibrer votre énergie intérieure. Nous offrons une
+        sélection d'activités pour répondre à vos besoins et aspirations, que
+        vous cherchiez l'aventure, la contemplation ou le calme.
       </p>
-      <p className="pb-10 px-6 text-justify md:mx-40">
-        Nous vous guidons pour une expérience enrichissante, sécurisée et
-        respectueuse de l'environnement. Découvrez les trésors naturels aux
-        portes de Paris avec CheminVert.
+      <p className="pb-10 px-10 text-justify md:mx-40">
+        Nous vous guidons à travers des escapades en pleine nature, pour nourrir
+        votre bien-être physique et mental. Nous vous fournissions toutes les
+        informations nécessaires pour que votre expérience soit enrichissante,
+        sécurisée et respectueuse de l'environnement.
+      </p>
+      <p className="pb-10 px-10 text-justify md:mx-40">
+        Venez explorer avec nous les trésors naturels aux portes de Paris et
+        découvrez un monde de sérénité et de découverte."
       </p>
       <div className="block md:hidden">
-      <h1 className="text-center text-3xl font-bold mb-6 pt-6">
-        Activités en vedettes
-      </h1>
-      <Carrousel />
+        <h1 className="text-center text-3xl font-bold mb-6 pt-6">
+          Activités en vedettes
+        </h1>
+        <Carrousel />
       </div>
       <div className="hidden md:block">
         <HomeActivity />
