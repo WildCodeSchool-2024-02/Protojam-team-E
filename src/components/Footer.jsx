7a1261ed7a2d97a347b6cce413ded496c3346953
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-05-17 09:28:07 +0400

    Add css for footer in desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
     <footer className="bg-gray-300 bg-opacity-30 border-t border-green-700 text-black mt-5 py-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src="/src/assets/logoNav.png" alt="Chemin Vert Logo" className="h-8 mr-2" />
-          <img className="w-24 ml-5" src="/src/assets/logoNavmilieu.png" alt="Chemin Vert Logo"/>
+          <img src="/src/assets/logoNav.png" alt="Chemin Vert Logo" className="h-8 mr-2 md:h-12" />
+          <img className="w-24 ml-5 md:w-60 md:ml-20" src="/src/assets/logoNavmilieu.png" alt="Chemin Vert Logo"/>
           </div>
         <div className="text-sm">© 2024 All rights reserved.</div>
       </div>
